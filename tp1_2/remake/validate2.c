44139f78cf1d91f1672c077a627d1cf17f2f9270
Gabriel La Torre <soy@latorregabriel.com>
2014-11-05 00:35:09 -0300

    Mejorando el remake del validate

--- tp1_2/remake/validate2.c
+++ tp1_2/remake/validate2.c
@@ -6,34 +6,63 @@
 #include "validate2.h"
 
 int validate(char *text, char **errmsg){
-	int error = 0;
-	while ((*text != EOF) && (error != 0)){
-		validateRecursivo(NULL,text, **errmsg);
-	}
-	printf("%c",*text);
+	//es posible que la asignacion &text no sea valida, o sea valida pero sea ilegal cambiar ese valor dentro de las funciones.
+	// de ser asi se deberia hacer:
+	char** pointerToText = &text; //y pasar este valor a validateRecursivo
+	int error = validateRecursivo(NULL,pointerToText, **errmsg);
+	
+// Estas lineas estan aca para mostrar como se puede leer e imprimir cada linea del char* solo avanzando el puntero de a uno.	
+	//printf("%c",*text);
+	//text++;
+	//printf("%c",*text);
 	return 0;
 }
 
-int validateRecursivo(char* currentTag, char* text, char **errmsg) {
+// Es necesario pasarle el valor del puntero al texto para que al avanzar en la lectura, este avance valga para todas
+// las llamadas a la funcion. Sino al  terminar una llamada, se seguiria del ultimo caracter despues de llamar.
+int validateRecursivo(char* currentTag, char** pointerToText, char **errmsg) {
 	char* newTag;
+	char* text = *pointerToText;
 	int resultadoRecursivo = 0;
 	// si currentTag es NULL entonces no estoy buscando un tag	
 	while (*text != EOF){
-		if (*text == "<"){
-			newTag = readTag(text);
+		if (*text == '<'){
+			newTag = readTag(pointerToText);
 			if (isClosingTag(newTag)){
 				return analizeMatch(currentTag, newTag, errmsg);	
 			} else {
-				 resultadoRecursivo = validateRecursivo(newTag, text, errmsg);
+				 resultadoRecursivo = validateRecursivo(newTag, pointerToText, errmsg);
 				//si no hay error sigo como si nada viendo los carateres que siguen
 				//si hay error tengo que ver si el tag que yo busco aparecio en algun momento	
 			}	
 		}else {
-			*text++; //avanzo en el texto
+			*pointerToText++; //avanzo en el texto
 		}		
 	}	
 	// si encuentro un cierre veo si es el que estoy buscando
 	// si es el que busco devuelvo codigo de exito
 	// si no es el que busco devuelvo codigo de error
 
+}
+
+int isClosingTag(char* tag) {
+	return ('/' == tag[0]);
+}
+
+char* readTag(char** pointerToText) {
+	char tag[20] = "";
+	char* text = *pointerToText;
+	int i=0;
+	while (*text != '>') {
+		text[i] = *text;
+		i++;
+		text++;	
+	}	
+	return tag;
+}
+
+int analizeMatch(char* currentTag, char* foundTag, char** errmsg) {
+	//si currentTag == foundTag (habiendole sacado el / inicial) entonces todo bien
+	// sino hay que ver en que caso de error estamos
+	return 0;
 }
--- tp1_2/remake/validate2.h
+++ tp1_2/remake/validate2.h
@@ -5,5 +5,9 @@
 
 //int validateRecursivo(char * text, int *pos , int *contador, char vTag[10][10], int * lineaError);
 int validate(char *text, char **errmsg);
+int validateRecursivo(char* currentTag, char* text, char **errmsg);
+int isClosingTag(char* tag);
+char* readTag(char* text);
+int analizeMatch(char* currentTag, char* foundTag, char** errmsg);
 
 #endif /* VALIDATE_H_ */
